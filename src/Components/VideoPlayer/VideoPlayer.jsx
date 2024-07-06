1a48e6aa6138b1d883787789662a507a115b5c69
Nirdosh2004 <nirdoshkushwaha75@gmail.com>
2024-07-06 18:57:59 +0530

    Completed

---
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+// import './VideoPlayer.css'
+// import video from '../../assets/video-college.mp4'
+
+// const VideoPlayer = ({playState,setPlayState}) => {
+
+// const player = useRef(null);
+
+// const closePlayer = (e) => {
+//           if(e.target === player.current){
+//                     setPlayState(false);
+//           }
+// }
+
+//   return (
+//     <div className={`video-player ${playState?'':'hide'}`} ref={player} onClick={closePlayer}>
+//           <video src={video} autoplay muted controls></video>
+//     </div>
+//   )
+// }
+
+// export default VideoPlayer
