2315f8a457469ae8019c4d0848097acfd580895c
Nirdosh2004 <nirdoshkushwaha75@gmail.com>
2024-07-06 12:06:07 +0530

    Next day

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
+import Testimonials from './Components/Testimonials/Testimonials'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
@@ -14,6 +18,13 @@ const App = () => {
         <Title subTitle='OUR PROGRAMS' title='What We Offer'/> 
       <Programs/>
       <About/>
+      <Title subTitle='Gallery' title='Campus Photos'/> 
+      <Campus/>
+      <Title subTitle='TESTIMONIALS' title='What Student Says'/> 
+      <Testimonials/>
+      <Title subTitle='Contact Us' title='Get in touch'/> 
+      <Contact/>
+      <Footer/>
       </div>
       
     </div>
